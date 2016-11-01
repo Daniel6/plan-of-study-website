241ceb3ec8d8bef80192be0afeea4baa44c41a28
dbishop <dbishop.rwc@gmail.com>
2016-11-01 09:04:40 -0400

    single column layout test

---
+++ public/scripts/main.js
@@ -0,0 +1,70 @@
+var app = angular.module("pypsApp", [])
+
+app.controller("PlanningController", function($scope) {
+	$scope.availableClasses = [
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		},
+		{
+			name: "Class 1", 
+			credits: 4, 
+			desc: "M, Th 1:30-3:20. AC 414. Short description goes here."
+		}
+	];
+});
+
+$(document).ready(function () {
+	
+});
